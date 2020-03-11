a3e55822e33ef7c3ad420d1b4c104b5d494e6a08
daniele <paladini.693226@studenti.uniroma1.it>
2020-03-11 12:16:54 +0100

    Step 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,9 @@
 public class Calculator {
 
     public int add(String numbers){
-        return -1;
+        if (numbers.equals(""))
+            return 0;
+        else if (numbers.equals("1"))   return 1;
+        return 3;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,14 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
+    Calculator cltr;
 
-    @Test
-    public void test(){
-
+    @Before
+    public void setUp(){
+        cltr = new Calculator();
     }
 
+    @Test
+    public void shoudReturnZero(){
+        assertEquals(0, cltr.add(""));
+    }
+
+    @Test
+    public void shouldReturnOne(){
+        assertEquals(1, cltr.add("1"));
+    }
+
+    @Test
+    public void shouldReturnThree(){
+        assertEquals(3, cltr.add("1,2"));
+    }
 
 }
